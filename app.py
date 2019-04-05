6e432e7e17eaa9d8617e37761996ebdec2c81c55
sushen <sushenbiswasaga@gmail.com>
2019-04-05 08:04:36 +0600

    Follow Pymessanger to send massage before that extract data from json object.

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, request
 from pprint import pprint
 from pymessenger import Bot
 
+app = Flask(__name__)
+
 
 Facebook_Access_Token = 'EAACvvGBVPhEBAIpqcxK5NkKZCa2Bmwlrblp01Tora2BFvLqCNhxmYoxoeVYvyiaxIfW05TL4BCPMlxT4QjzDsaUumtHZCcnhvExSoa6y5I6ATmHJJaDPj4b7rZAtPZAbftEA5vgcCKuYj9xzn0twsQUEZBrMybJZCIPFQCdvx60QZDZD'
+
 bot = Bot(Facebook_Access_Token)
 
-app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -29,28 +31,31 @@ def webhook():
     data = request.get_json()
     log(data)
 
-    if data['object'] == ['page']:
+    # Necessary Code that extract json data facebook send
+    if data['object'] == 'page':
         for entry in data['entry']:
-            for messaging_event in data ['messaging']:
+            for messaging_event in entry['messaging']:
 
+                # IDs
                 sender_id = messaging_event['sender']['id']
                 recipient_id = messaging_event['recipient']['id']
 
                 if messaging_event.get('message'):
+                    # Extracting text message
                     if 'text' in messaging_event['message']:
                         messaging_text = messaging_event['message']['text']
                     else:
-                        messaging_text = 'No text'
+                        messaging_text = 'no text'
 
-                response = messaging_text
-                bot.send_text_message(sender_id, response)
-
-
+                    # Echo Bot
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
 
     return "ok", 200
 
 
 def log(message):
+    # previously it was print now I just Use Petty Print
     pprint(message)
     sys.stdout.flush()
 
