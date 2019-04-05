00e4959164d9ea517be141f3914a4e4ae73af3e6
sushen <sushenbiswasaga@gmail.com>
2019-04-05 08:34:41 +0600

    Follow Pymessanger to send massage before that extract data from json object.

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from flask import Flask, request
 from pprint import pprint
 from pymessenger import Bot
 
-app = Flask(__name__)
-
-
 Facebook_Access_Token = 'EAACvvGBVPhEBAIpqcxK5NkKZCa2Bmwlrblp01Tora2BFvLqCNhxmYoxoeVYvyiaxIfW05TL4BCPMlxT4QjzDsaUumtHZCcnhvExSoa6y5I6ATmHJJaDPj4b7rZAtPZAbftEA5vgcCKuYj9xzn0twsQUEZBrMybJZCIPFQCdvx60QZDZD'
 
 bot = Bot(Facebook_Access_Token)
 
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
